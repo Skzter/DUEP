4f8e8877a0d408be4d2cacaf9f8a16bcfc24ad7d
Skzter <v.goetze03@gmail.com>
2024-11-12 01:09:17 +0100

    started chunk function

--- stdin.cpp
+++ stdin.cpp
@@ -1,10 +1,21 @@
 #include<iostream>
 #include<string>
 
+#define CHUNK_SIZE 64
+
 std::string ReadBinary();
+int NumberOfChunks(std::string input);
+
 int main()
 {
-    std::cout << ReadBinary() << "\nEnd of File";
+    std::string input = ReadBinary();
+    std::cout << input.length() << std::endl;
+    std::cout << input.size() << std::endl;
+    std::cout << NumberOfChunks(input) << std::endl;
+    double test = (double)312/64;
+    std::cout.precision(4);
+    std::cout.setf(std::ios::fixed);
+    std::cout << test << "test" << std::endl;
 }
 
 std::string ReadBinary()
@@ -12,4 +23,11 @@ std::string ReadBinary()
     std::string eingabe;
     std::getline(std::cin, eingabe);
     return eingabe;
+}
+
+int NumberOfChunks(std::string input)
+{
+    double t = input.length()/CHUNK_SIZE;
+    std::cout << "This is with half chunks: " << t << std::endl;
+    return input.length()/CHUNK_SIZE;
 }
