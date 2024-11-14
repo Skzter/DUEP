d5626a674084fdf421b59e95f7df64c0af668117
Skzter <v.goetze03@gmail.com>
2024-11-14 13:42:39 +0100

    added chunk representation and functions for receiving messages

--- stdin.cpp
+++ stdin.cpp
@@ -1,11 +1,19 @@
 #include<iostream>
 #include<string>
+#include<vector>
 
 #define CHUNK_SIZE 64.0
 
+//Send Message
 std::string InputToBinary(std::string InputString);
 std::string IntToBinary(int Input);
 double NumberOfChunks(std::string InputBinary);
+void PrintChunks(std::string InputBinary);
+
+//Receive Message
+std::vector<int> BinaryToInt(std::string InputBinary); //8bit to int -- save in vector 
+std::string IntToChar(std::vector<int> InputInt); //int to char -- save in string
+void PrintMessage(std::string InputString); //print message, number of chunks received
 
 int main()
 {
@@ -14,7 +22,9 @@ int main()
     std::string InputConverted = InputToBinary(Input);
     std::cout << InputConverted << std::endl;
     std::cout << NumberOfChunks(InputConverted) << std::endl;
- }
+    PrintChunks(InputConverted);
+}
+
 
 std::string InputToBinary(std::string InputString)
 {
@@ -44,9 +54,20 @@ std::string IntToBinary(int Input)
     return Binary;
 }
 
+void PrintChunks(std::string InputBinary)
+{
+    long unsigned int NumChunks = NumberOfChunks(InputBinary);
+    for(size_t i = 0; i <  NumChunks; i++)
+    {
+        std::string chunk = InputBinary.substr(64*i, 64);
+        std::cout << chunk << std::endl;
+    }
+    std::cout << InputBinary.substr(64*NumChunks, InputBinary.length() - 64*NumChunks) << std::endl;
+
+    std::cout << "Das sind die Chunks" << std::endl;
+}
+
 double NumberOfChunks(std::string InputBinary)
 {
     return InputBinary.length()/CHUNK_SIZE;
 }
-
-
