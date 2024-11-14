a5b6ab913214962a96cc8450448ccfb0d4ba83bd
Skzter <v.goetze03@gmail.com>
2024-11-14 16:51:24 +0100

    chunk function complete + decoding chunk function complete

--- stdin.cpp
+++ stdin.cpp
@@ -1,28 +1,42 @@
 #include<iostream>
 #include<string>
 #include<vector>
+#include<math.h>
 
-#define CHUNK_SIZE 64.0
+#define CHUNK_SIZE 64
 
 //Send Message
 std::string InputToBinary(std::string InputString);
 std::string IntToBinary(int Input);
+void MakeChunks(std::string InputBinary);
 double NumberOfChunks(std::string InputBinary);
-void PrintChunks(std::string InputBinary);
+void PrintSendChunks(std::string InputBinary);
 
 //Receive Message
-std::vector<int> BinaryToInt(std::string InputBinary); //8bit to int -- save in vector 
+void SaveBinaryInInt(std::string InputBinary); //whole Message in Int -- save in vector 
 std::string IntToChar(std::vector<int> InputInt); //int to char -- save in string
 void PrintMessage(std::string InputString); //print message, number of chunks received
+
+//Global Variables
+std::vector<std::string> ChunksSend;
+std::vector<int> ReceivedBinaryToInt;
 
 int main()
 {
     std::string Input;
     std::getline(std::cin, Input);
     std::string InputConverted = InputToBinary(Input);
-    std::cout << InputConverted << std::endl;
-    std::cout << NumberOfChunks(InputConverted) << std::endl;
-    PrintChunks(InputConverted);
+    MakeChunks(InputConverted);
+    PrintSendChunks(InputConverted);
+    std::cout << std::endl;
+
+    SaveBinaryInInt(InputConverted);
+    std::cout << "Ergebnis > ";
+    for(size_t i = 0; i < ReceivedBinaryToInt.size(); i++)
+    {
+        std::cout << static_cast<char>(ReceivedBinaryToInt.at(i));
+    }
+    std::cout << std::endl;
 }
 
 
@@ -31,7 +45,8 @@ std::string InputToBinary(std::string InputString)
     std::string InputInBinary;
     for(size_t i = 0; i < InputString.length(); i++)
     {
-        int Converted = static_cast<int>(InputString[i]);
+        unsigned char test = static_cast<unsigned char>(InputString[i]);
+        int Converted = static_cast<int>(test);
         InputInBinary += IntToBinary(Converted);
     }
     return InputInBinary;
@@ -50,24 +65,60 @@ std::string IntToBinary(int Input)
         Binary = std::to_string(Remainder) + Binary;
         Input = Quotient;
     }while(Quotient != 0);
-
+    
+    if(Binary.length() < 8)
+    {
+        long unsigned int temp = 8-Binary.length();
+        for(size_t i = 0; i < temp; i++)
+        {
+            Binary = "0" + Binary;
+        }
+    }
     return Binary;
 }
 
-void PrintChunks(std::string InputBinary)
+void MakeChunks(std::string InputBinary)
 {
     long unsigned int NumChunks = NumberOfChunks(InputBinary);
-    for(size_t i = 0; i <  NumChunks; i++)
-    {
-        std::string chunk = InputBinary.substr(64*i, 64);
-        std::cout << chunk << std::endl;
-    }
-    std::cout << InputBinary.substr(64*NumChunks, InputBinary.length() - 64*NumChunks) << std::endl;
 
-    std::cout << "Das sind die Chunks" << std::endl;
+    for(size_t i = 0; i < NumChunks; i++)
+    {
+        ChunksSend.push_back(InputBinary.substr(CHUNK_SIZE*i, CHUNK_SIZE));
+    }
+
+
+    if(CHUNK_SIZE*NumChunks < InputBinary.length())
+    {
+        unsigned long int Remainder = InputBinary.length()-CHUNK_SIZE*NumChunks;
+
+        for(size_t i = 0; i < CHUNK_SIZE-Remainder; i++)
+        {
+            InputBinary += "0";
+        }
+
+        ChunksSend.push_back(InputBinary.substr(CHUNK_SIZE*NumChunks, CHUNK_SIZE));
+    }
+}
+
+void PrintSendChunks(std::string InputBinary)
+{
+    for(size_t i = 0; i < ChunksSend.size(); i++)
+    {
+        std::cout << ChunksSend.at(i) << std::endl; 
+    }
 }
 
 double NumberOfChunks(std::string InputBinary)
 {
     return InputBinary.length()/CHUNK_SIZE;
 }
+
+void SaveBinaryInInt(std::string InputBinary)
+{
+    for(size_t i = 0; i < InputBinary.length(); i = i+8)
+    {
+        std::string byte = InputBinary.substr(i, 8);
+        ReceivedBinaryToInt.push_back(std::stoi(byte, 0, 2));
+    }
+}
+        
